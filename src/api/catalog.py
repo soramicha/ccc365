7ad012a17622f8614b367d9beb59c298a1d1bbd0
soramicha <schang7jcu@gmail.com>
2024-04-12 13:14:10 -0700

    Completed Version 1

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -18,7 +18,7 @@ def get_catalog():
                 {
                     "sku": "GREEN_POTION_0",
                     "name": "green potion",
-                    "quantity": num,
+                    "quantity": 1,
                     "price": 50,
                     "potion_type": [0, 100, 0, 0],
                 }

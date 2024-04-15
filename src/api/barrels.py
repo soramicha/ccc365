c9fabd8631e15e053947797b7b5138aeaae05a03
soramicha <schang7jcu@gmail.com>
2024-04-15 13:30:21 -0700

    Version 2 Complete

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -22,11 +22,11 @@ class Barrel(BaseModel):
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     with db.engine.begin() as connection:
-        if barrels_delivered[0].sku == "SMALL_BLUE_BARREL":
+        if barrels_delivered[0].sku == "SMALL_BLUE_BARREL" or barrels_delivered[0].sku == "MINI_BLUE_BARREL":
             connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {barrels_delivered[0].price}, blue_ml = blue_ml + {barrels_delivered[0].ml_per_barrel}"))
-        if barrels_delivered[0].sku == "SMALL_RED_BARREL":
+        if barrels_delivered[0].sku == "SMALL_RED_BARREL" or barrels_delivered[0].sku == "MINI_RED_BARREL":
             connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {barrels_delivered[0].price}, red_ml = red_ml + {barrels_delivered[0].ml_per_barrel}"))
-        if barrels_delivered[0].sku == "SMALL_GREEN_BARREL":
+        if barrels_delivered[0].sku == "SMALL_GREEN_BARREL" or barrels_delivered[0].sku == "MINI_GREEN_BARREL":
             connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = gold - {barrels_delivered[0].price}, num_green_ml = num_green_ml + {barrels_delivered[0].ml_per_barrel}"))
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
@@ -44,30 +44,52 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         result = connection.execute(sqlalchemy.text('SELECT gold FROM global_inventory'))
     
     gold = result.fetchone()[0]
-    if blue.fetchone()[0] < 1 and gold >= 120:
+    if gold < 60:
+        return [
+            {
+                "sku": "SMALL_GREEN_BARREL",
+                "quantity": 0,
+            }
+        ]
+    if blue.fetchone()[0] < 1 and gold >= 60:
+        return [
+            {
+                "sku": "MINI_BLUE_BARREL",
+                "quantity": 1,
+            }
+        ]
+    elif red.fetchone()[0] < 1 and gold >= 60:
+        return [
+                {
+                    "sku": "MINI_RED_BARREL",
+                    "quantity": 1,
+                }
+            ]
+    elif green.fetchone()[0] < 1 and gold >= 60:
+        return [
+            {
+                "sku": "MINI_GREEN_BARREL",
+                "quantity": 1,
+            }
+        ]
+    elif blue.fetchone()[0] < 1 and gold >= 120:
         return [
             {
                 "sku": "SMALL_BLUE_BARREL",
                 "quantity": 1,
             }
         ]
-    if red.fetchone()[0] < 1 and gold >= 100:
+    elif red.fetchone()[0] < 1 and gold >= 100:
         return [
                 {
                     "sku": "SMALL_RED_BARREL",
                     "quantity": 1,
                 }
             ]
-    if green.fetchone()[0] < 1 and gold >= 100:
+    elif green.fetchone()[0] < 1 and gold >= 100:
         return [
             {
                 "sku": "SMALL_GREEN_BARREL",
                 "quantity": 1,
-            }
-        ]
-    return [
-            {
-                "sku": "SMALL_GREEN_BARREL",
-                "quantity": 0,
             }
         ]

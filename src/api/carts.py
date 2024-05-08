89bb7058254b3f925be0f2be4d7cf814d40f9dec
soramicha <schang7jcu@gmail.com>
2024-05-08 15:49:43 -0700

    Update carts.py

--- src/api/carts.py
+++ src/api/carts.py
@@ -26,67 +26,64 @@ class search_sort_order(str, Enum):
 def search_orders(
     customer_name: str = "",
     potion_sku: str = "",
-    search_page: str = "",
+    search_page: str = "1",
     sort_col: search_sort_options = search_sort_options.timestamp,
     sort_order: search_sort_order = search_sort_order.desc,
 ):
     
+    search_page = int(search_page)
+    sql = "SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders "
+    # both name and potion name are used
+    if potion_sku != "" and customer_name != "":
+        sql += "WHERE potion_name = " + potion_sku + " AND customer = " + customer_name
+    elif potion_sku != "":
+        sql += "WHERE potion_name = " + potion_sku
+    elif customer_name != "":
+         sql += "WHERE customer = " + customer_name
+    
+    # sort through time
+    if sort_col == search_sort_options.timestamp:
+        sql += " ORDER BY time "
+    # sort through customer name
+    elif sort_col == search_sort_options.customer_name:
+        sql += " ORDER BY customer "
+    # sort through potion name
+    elif sort_col == search_sort_options.item_sku:
+        sql += " ORDER BY potion_name "
+    # sort through gold
+    elif sort_col == search_sort_options.line_item_total:
+        sql += " ORDER BY gold "
+        
+    if sort_order == search_sort_order.asc:
+        sql += " ASC"
+    else:
+        sql += "DESC"
+    
+    offset = 5 * search_page
+    sql += " OFFSET " + str(offset) + " LIMIT 5"
     result = []
     Dict = {}
+    # execute the command
     with db.engine.begin() as connection:
-        if customer_name == "" and potion_sku == "":
-            # ASC ORDER
-            if sort_order == search_sort_order.asc:
-                # TIME
-                if sort_col == search_sort_options.timestamp:
-                    print("sorting by time asc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY time ASC"))
-                elif sort_col == search_sort_options.line_item_total: # LINE_ITEM_TOTAL
-                    print("sorting by line_item asc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY gold ASC"))
-                elif sort_col == search_sort_options.item_sku: # ITEM_SKU
-                    print("sorting by item_sku asc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY potion_name ASC"))
-                elif sort_col == search_sort_options.customer_name: # NAME
-                    print("sorting by name asc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY customer ASC"))
-                else: # CART_ID
-                    print("sorting by cart_id asc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY cart_id ASC"))
-            
-        # DESC ORDER 
-            elif sort_order == search_sort_order.desc:
-                # TIME
-                if sort_col == search_sort_options.timestamp:
-                    print("sorting by time desc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY time DESC"))
-                elif sort_col == search_sort_options.line_item_total: # LINE_ITEM_TOTAL
-                    print("sorting by line_item desc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY gold DESC"))
-                elif sort_col == search_sort_options.item_sku: # ITEM_SKU
-                    print("sorting by item_sku desc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY potion_name DESC"))
-                elif sort_col == search_sort_options.customer_name: # NAME
-                    print("sorting by name desc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY customer DESC"))
-                else: # CART_ID
-                    print("sorting by cart_id desc!")
-                    rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders ORDER BY cart_id DESC"))
-        else:
-            if potion_sku != "" and customer_name != "":
-                rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders WHERE UPPER(customer) LIKE UPPER(:name) AND UPPER(potion_name) LIKE UPPER(:p_name) ORDER BY potion_name, customer"), [{"name": customer_name, "p_name": potion_sku}])
-            elif potion_sku != "":
-                rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders WHERE UPPER(potion_name) LIKE UPPER(:p_name) ORDER BY potion_name"), [{"p_name": potion_sku}])
-            elif customer_name != "":
-                rows = connection.execute(sqlalchemy.text("SELECT cart_id, customer, potion_id, gold, time, potion_name FROM search_orders WHERE UPPER(customer) LIKE UPPER(:name) ORDER BY customer"), [{"name": customer_name}])
-
-        # get the starting id in cart_items
+        rows = connection.execute(sqlalchemy.text(sql))
+        count = connection.execute(sqlalchemy.text("SELECT COUNT(potion_name) FROM search_orders"))
+        count = count.fetchone()[0]
         for i in rows.fetchall():
             # FIX AMOUNT OF POTIONS LATER
             Dict = dict({"line_item_id": i[0], "item_sku": "1 " + i[5], "customer_name": i[1], "line_item_total": i[3], "timestamp": i[4]})
             result.append(Dict)
     print(result)
-        
+    
+    prev = 1
+    next = 1
+    if search_page > 1 and search_page < int(count/5):
+        next = search_page + 1
+        prev = search_page - 1
+    elif search_page == 1:
+        next = search_page + 1
+    elif search_page > 1 and search_page == int(count/5):
+        prev = search_page - 1
+
     """
     Search for cart line items by customer name and/or potion sku.
 
@@ -113,8 +110,8 @@ def search_orders(
     """
 
     return {
-        "previous": "",
-        "next": "",
+        "previous": prev,
+        "next": next,
         "results": result,
     }
 
